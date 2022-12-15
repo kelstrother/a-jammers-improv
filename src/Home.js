f1134b5586b2b667df9ca34cbfdb3d3d3d826d56
Kel Strother <kelstrother@gmail.com>
2022-12-15 10:54:44 -0700

    added functionality for a user to delete an entry or improv

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,20 @@ import BlogList from './BlogList'
 
 export default function Home() {
   const [blogs, setBlogs] = useState([
-    { title: 'How to cope with being the best.', body: 'lorem ipsum...', author: 'Jaco', id: 1 },
-    { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'Stasik', id: 2 },
-    { title: 'Being old and in a jamband...', body: 'lorem ipsum...', author: 'Joel', id: 3 }
+    { title: 'How to cope with being the best.', body: 'lorem ipsum...', author: 'jaco', id: 1 },
+    { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'jaco', id: 2 },
+    { title: 'Being old and in a jamband...', body: 'lorem ipsum...', author: 'joel', id: 3 }
   ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs)
+  }
 
   return (
     <div className="home">
-     <BlogList blogs={blogs} title="All Improv!" />
+     <BlogList blogs={blogs} title="All Improv!" handleDelete={handleDelete} />
+     {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'jaco')} title="Jacos Improv" /> */}
     </div>
   );
 }
